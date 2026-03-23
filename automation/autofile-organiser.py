e3f6f282937a921407b25a5c71a71a0fc9c2916d
Anuja khatri <khatrianuja20@gmail.com>
2026-03-23 08:48:30 +0545

    class and methods updated

---
+++ automation/autofile-organiser.py
@@ -0,0 +1,11 @@
+#automation tool for auto file organizer
+import os
+path_to_organize = "/Users/anujakhatri/Desktop"
+
+file_types = {
+    "Images" : [".jpg", ".png", ".jpeg"],
+    "Documents" :[".pdf", ".zip"] ,
+    "Videos": [],
+    "Archives": [".zip", '.tar', '.tar.gz' ]
+}
+
