aace3f5f26a79e22a200e613d9d1410551afa891
Anuja khatri <khatrianuja20@gmail.com>
2026-04-11 13:48:25 +0545

    add patient and appointment model in clinic

--- smart_clinic/clinic/models.py
+++ smart_clinic/clinic/models.py
@@ -1,11 +1,39 @@
 from django.db import models
+from django.template.defaultfilters import length
+
 
 # Create your models here.
 class Doctor(models.Model):
-    Name = models.CharField(max_length=40)
-    Specialization = models.CharField(max_length=100)
-    Experience = models.CharField(max_length=50)
+    name = models.CharField(max_length=40)
+    specialization = models.CharField(max_length=100)
+    experience = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.Name
+        return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+    contact = models.IntegerField(max_length=15)
+    reason = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model) :
+    APPOINTMENT_OPTIONS = [
+        ('pending', 'Pending'), #(Raw database, Human Readable)
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+        ('completed', 'Completed'),
+    ]
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, related_name='appointments')
+    patient = models.ForeignKey(Patient, on_delete =models.CASCADE, related_name='appointments')
+    date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices = APPOINTMENT_OPTIONS, default='pending')
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.patient} → {self.doctor} on {self.date} "
+
 
--- smart_clinic/smart_clinic/settings.py
+++ smart_clinic/smart_clinic/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'clinic',
     'rest_framework',
+    # 'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
