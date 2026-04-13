c009bbbea94863585309f053b9300732962827d8
Anuja khatri <khatrianuja20@gmail.com>
2026-04-13 22:32:29 +0545

    add login and register view

--- smart_clinic/frontend/views.py
+++ smart_clinic/frontend/views.py
@@ -4,5 +4,9 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
+def login_page(request):
+    return render(request,'login.html')
 
+def register_page(request):
+    return render(request, 'register.html')
 
