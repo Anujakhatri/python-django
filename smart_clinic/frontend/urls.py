c2591e47f54256a1fbb41574393bb3b9b2ca4141
Anuja khatri <khatrianuja20@gmail.com>
2026-04-13 22:33:11 +0545

    add a urls of login and register

--- smart_clinic/frontend/urls.py
+++ smart_clinic/frontend/urls.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import login
 from django.urls import path
-from .views import home
+from .views import home, login_page, register_page
 
 urlpatterns = [
     path('', home),
+    path('login/', login_page),
+    path('register/', register_page),
 ]
